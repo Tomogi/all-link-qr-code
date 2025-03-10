b918a88e3751d8a2ba5f20df7592d9e5728d49aa
trananh-tt-intervideo <32162425+sammytran90@users.noreply.github.com>
2025-03-10 15:51:20 +0700

    list

--- webClient/islands/List.tsx
+++ webClient/islands/List.tsx
@@ -1,25 +1,60 @@
+import { Button } from "../components/Button.tsx";
+import { signal } from "@preact/signals";
+import { useState } from "preact/hooks";
 import { TextInput } from "../components/TextInput.tsx";
 
 interface LinkListProps {}
 
-export default function LinkList(props: LinkListProps) {
-  const itemsMap = new Map<string, string>();
+const urls = signal<string[]>([]);
+const test = signal<number>(0);
+
+(function () {
   for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-    if (key) {
-      itemsMap.set(key, localStorage.getItem(key) || "");
+    const url = localStorage.getItem(i.toString());
+    if (url) {
+      urls.value.push(url);
     }
   }
+})();
+
+export default function LinkList(props: LinkListProps) {
+  const [addingUrl, setAddingUrl] = useState("");
+  function addUrl(url: string) {
+    localStorage.setItem(urls.value.length.toString(), url);
+    urls.value.push(url);
+  }
+
+  function removeUrl(index: number) {
+    urls.value = urls.value.filter((_, _index) => index !== _index);
+    localStorage.removeItem(index.toString());
+  }
+
   return (
-    <div>
-      <ol>
-        {Array.from(itemsMap.entries()).map(([key, value]) => (
-          <li key={key}>
-            <TextInput value={value} readOnly />
-          </li>
-        ))}
-        <TextInput />
-      </ol>
-    </div>
+    <ul>
+      {urls.value.map((item, index) => (
+        <li key={item}>
+          <TextInput value={item} readOnly />
+          <Button
+            onClick={() => {
+              removeUrl(index);
+            }}
+          >
+            Delete
+          </Button>
+        </li>
+      ))}
+      <li>
+        <TextInput value={addingUrl} onChange={(e) => setAddingUrl(e.target.value)} />
+
+        <Button
+          onClick={() => {
+            addUrl(addingUrl);
+            setAddingUrl("");
+          }}
+        >
+          Add
+        </Button>
+      </li>
+    </ul>
   );
 }
